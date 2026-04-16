dc2f7fe4bacfa61fff6c89bb9917346dfae14df8
Sebastian Rodriguez <158363727+4rleo@users.noreply.github.com>
2026-04-16 09:02:10 -0600

    Actualizar Coursera_pricing_tracker.py

--- Coursera_pricing_tracker.py
+++ Coursera_pricing_tracker.py
@@ -39,7 +39,8 @@ def get_price(options):
     options.binary_location = os.getenv("CHROME_BIN")
     service = Service(os.getenv("CHROMEDRIVER_BIN"))
     driver = webdriver.Chrome(service=service, options=options)
-    try: driver.get("https://www.coursera.org/courseraplus/special/latam-spring-2026-40")
+    try:
+ driver.get("https://www.coursera.org/courseraplus/special/latam-spring-2026-40")
         wait = WebDriverWait(driver, 20)
         wait.until(EC.presence_of_element_located((By.CLASS_NAME, "rc-ReactPriceDisplay")))
         

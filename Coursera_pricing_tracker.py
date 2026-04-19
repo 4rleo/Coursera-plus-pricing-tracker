9914b2b0a390e0e09df72e0d378ddf6e8caf56e3
Sebastian Rodriguez <158363727+4rleo@users.noreply.github.com>
2026-04-18 22:41:31 -0600

    Update Coursera_pricing_tracker.py

--- Coursera_pricing_tracker.py
+++ Coursera_pricing_tracker.py
@@ -13,7 +13,6 @@ import time
 
 def main():
     options = Options()
-    options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--window-size=1920,1080")

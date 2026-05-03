3ea5bf642bd68fd4a0cc791cd20fcf625e03e434
Sebastian Rodriguez <158363727+4rleo@users.noreply.github.com>
2026-05-03 08:50:14 -0600

    Update Coursera_pricing_tracker.py

--- Coursera_pricing_tracker.py
+++ Coursera_pricing_tracker.py
@@ -82,8 +82,7 @@ def intercept_stripe_url(clean_cookies):
         page = context.new_page()
 
         page.goto("https://www.coursera.org/courseraplus")
-        text = page.locator(".css-j90x6z").inner_text()
-        print(text)
+        
         try:
             page.click("button.css-j90x6z", timeout=10000)
             
